3dcd4a15244ee8197fe52d5949730254c486f31b
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-03-05 14:46:38 +0200

    Add files via upload

---
+++ Sp_ultra/del_events/EventsFriday/BankOfAmerica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFriday
+{
+    public class BankOfAmerica
+    {
+        public delegate void notify_delegate(double maded, string msg);
+        public notify_delegate NotifyMeWhenStocksChanged;
+
+        public void StockChanged()
+        {
+            double madad = 100 + 1.1 * 1.5435;
+
+            if (NotifyMeWhenStocksChanged != null)
+            {
+                NotifyMeWhenStocksChanged(madad, "changed by one stock!");
+            }
+        }
+    }
+}
---
+++ Sp_ultra/del_events/EventsFriday/StocksBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFriday
+{
+    public class StocksBoard
+    {
+        private void RedrawBoard(double maded, string msg)
+        {
+            Console.WriteLine($"Redrawring board with data: {maded} {msg}");
+        }
+
+        public StocksBoard(BankOfAmerica boa)
+        {
+            boa.NotifyMeWhenStocksChanged += RedrawBoard;
+        }
+    }
+}
